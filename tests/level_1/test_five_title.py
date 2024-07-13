d96410a9337fb7df8ce95cb8b8c682fcb37785e5
Татьяна Босачева <bosacheva98@mail.ru>
2024-07-13 14:48:47 +0300

    add parametrize version 2

--- tests/level_1/test_five_title.py
+++ tests/level_1/test_five_title.py
@@ -1,11 +1,18 @@
+import pytest
+
 from functions.level_1.five_title import change_copy_item
 
-
-def test_change_copy_item():
-    assert change_copy_item('Заголовок', 5) == 'Заголовок'
-    assert change_copy_item("100 самых интересных мест в России") == \
-        'Copy of 100 самых интересных мест в России'
-    assert change_copy_item("Copy of 100 самых интересных мест в России (3)") == \
-        'Copy of 100 самых интересных мест в России (4)'
-    assert change_copy_item("Copy of 100 самых интересных мест в России") == \
-        'Copy of 100 самых интересных мест в России (2)'
+@pytest.mark.parametrize(
+        "text, number, result", 
+        [
+            ('Заголовок', 5, 'Заголовок'),
+            ('100 самых интересных мест в России', None, 'Copy of 100 самых интересных мест в России'),
+            ('Copy of 100 самых интересных мест в России (3)', None, 'Copy of 100 самых интересных мест в России (4)'),
+            ('Copy of 100 самых интересных мест в России', None, 'Copy of 100 самых интересных мест в России (2)'),
+        ]
+)
+def test_change_copy_item(text, number, result):
+    if number:
+        assert change_copy_item(text, number) == result
+    else:
+        assert change_copy_item(text) == result
